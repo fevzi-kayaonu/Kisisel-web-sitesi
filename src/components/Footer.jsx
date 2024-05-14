16cf649ebc8da08069355015741ad51ebab06ecf
fevzi-kayaonu <fevzikayaonu35@gmail.com>
2024-05-14 23:21:19 +0300

    footer tamamlandı

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,35 @@
+const Footer = () => {
+  return (
+    <>
+      <footer className="padding-bottom-3">
+        <div className="flex js-center alg-center gap-4 padding-top-3 padding-bottom-3 ">
+          <div
+            className="flex column fs-800 fw-500 lh-4 padding-bottom-3"
+            style={{ alignItems: "flex-end" }}
+          >
+            <p>Let’s work together on</p>
+            <p>your next product.</p>
+          </div>
 
-
-
-const Footer  = () => {
-
-
-    return (
-        <>
-          Footer
-        </>
-      )
-
-}
+          <div className="flex column fs-600 fw-500 lh-2 padding-bottom-3">
+            <a href="/" className="">
+              Github
+            </a>
+            <a href="/" className="">
+              Personal Blog
+            </a>
+            <a href="/" className="">
+              {" "}
+              Linkedin
+            </a>
+            <a href="/" className="">
+              Email
+            </a>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
 
 export default Footer;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,28 +2,44 @@ import { projectsData } from "../data";
 
 const Projects = () => {
   return (
-    <section className="flex js-center bg-white">
-      <div className="flex column flex-basis-80 alg-center">
-        <h2>Projects</h2>
-        <div className="flex">
-          {projectsData.en.map((item) => (
-            <article className="flex-basis-30" key={item.title}>
-              <h3>{item.title}</h3>
-              <p>{item.content}</p>
-              <ul>
-                {item.skills.map((skill) => (
-                  <li key={skill}>{skill}</li>
-                ))}
-              </ul>
-              <div>
-                <a href="/">View on Github</a>
-                <a href="/">Go to app -</a>
-              </div>
-              <picture>
+    <section className="flex js-center bg-white  padding-top-3 padding-bottom-3">
+      <div className="flex column flex-basis-80 alg-center  padding-bottom-3 gap-2">
+        <h2 className="fs-800 fw-500 lh-3">Projects</h2>
+        <div className="flex js-center gap-4 ">
+          {projectsData.en.map((item, index) => (
+            <div className="flex column  flex-basis-40 " key={item.title}>
+              <article
+                className={`flex column gap-2 padding-top-2 padding-bottom-2 padding-left-1 padding-right-1 flex-basis-100 ${
+                  index % 2 == 0 ? "bg-blue" : "bg-green"
+                }`}
+                style={{
+                  paddingBottom: "15rem",
+                  position: "relative",
+                  borderRadius: "1rem",
+                }}
+              >
+                <h3 className="ff-play fw-700 fs-700 lh-3">{item.title}</h3>
+                <p className="fw-400 fs-500 ">{item.content}</p>
+                <ul className="flex wrap gap-1">
+                  {item.skills.map((skill) => (
+                    <li
+                      className=" bg-white ff-play fw-700 fs-500 lh-1"
+                      key={skill}
+                    >
+                      {skill}
+                    </li>
+                  ))}
+                </ul>
+                <div className="flex js-between fw-600 fs-550 lh-2">
+                  <a href="/">View on Github</a>
+                  <a href="/">Go to app -</a>
+                </div>
+              </article>
+              <picture className="">
                 <img src={item.images[0].src} alt={item.images[0].alt} />
                 <img src={item.images[1].src} alt={item.images[1].alt} />
               </picture>
-            </article>
+            </div>
           ))}
         </div>
       </div>
